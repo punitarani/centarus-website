b8e37e14ab1ba80bfab5a12b16c98eb0898b8093
Punit Arani <punitsai36@gmail.com>
2022-12-28 20:35:25 -0700

    Add title and tagline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Centarus</h1>
+<main>
+	<h1 class="title">Centarus</h1>
+	<h2 class="tagline">Investing in Humanity</h2>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		width: 100vw;
+		max-width: 100%;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-family: 'Hubot Sans', sans-serif;
+		font-size: 100px;
+		font-weight: 800;
+		text-align: center;
+		margin: 0;
+	}
+
+	.tagline {
+		font-family: 'Mona Sans', sans-serif;
+		font-size: 42px;
+		font-weight: 600;
+		text-align: center;
+		margin: 0;
+	}
+</style>
