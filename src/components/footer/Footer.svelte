41f3b0a9a09099c12f9b2f789bae61c37a3515f3
Punit Arani <punitsai36@gmail.com>
2022-12-30 00:21:20 -0700

    Create simple footer

---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,28 @@
+<footer>
+	<div class="footer-container">
+		<p>Centarus</p>
+	</div>
+</footer>
+
+<style>
+	footer {
+		height: 50px;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.footer-container {
+		height: 60px;
+		width: 90%;
+		max-width: 1080px;
+
+		box-sizing: border-box;
+		padding: 0 24px;
+
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+	}
+</style>
