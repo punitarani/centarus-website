b60e8f06bd3c11c8ee665fdc8fe7cd03af3bf209
Punit Arani <punitsai36@gmail.com>
2022-12-30 03:08:24 -0700

    Add email validation

--- src/routes/home/EmailForm.svelte
+++ src/routes/home/EmailForm.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	let email = '';
+	let errorMessage = '';
+
+	// validateEmail validates the email address
+	function validateEmail(email) {
+		const emailRegEx =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return emailRegEx.test(String(email).toLowerCase());
+	}
 
 	// submitEmail handles the form submission
 	function submitEmail() {
+		if (email.length == 0) {
+			errorMessage = 'Please enter an email address';
+		} else if (!validateEmail(email)) {
+			errorMessage = 'Please enter a valid email address';
+		} else {
+			errorMessage = '';
+		}
+
 		console.log(email);
+		console.log(errorMessage);
 	}
 </script>
 
