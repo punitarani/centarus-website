290633251107fc34d21b1cb2a0ee6f6614015b0d
Punit Arani <punitsai36@gmail.com>
2023-01-02 14:20:06 -0700

    Add margins and padding to footer and elements

--- src/components/footer/footer.svelte
+++ src/components/footer/footer.svelte
@@ -1,5 +1,6 @@
-<footer class="flex h-12 items-center justify-center">
-	<div class="flex h-10 w-10/12 max-w-screen-xl flex-row items-center justify-between">
+<footer class="mb-4 flex h-12 flex-col items-center justify-center">
+	<hr class="mb-2 h-px w-10/12 max-w-screen-xl rounded border-0 bg-gray-700" />
+	<div class="flex h-10 w-10/12 max-w-screen-xl flex-row items-center justify-between px-2">
 		<p>Centarus</p>
 		<a href="https://github.com/punitarani/centarus-website">
 			<img src="icons/github/github-mark-white.png" alt="GitHub logo" class="h-5" />
