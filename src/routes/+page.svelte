92b99bad785a7966f5cbe0c29d5079188876e3e8
Punit Arani <punitsai36@gmail.com>
2023-01-01 19:30:36 -0700

    Update tagline size to text-3xl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 
 <main class="flex h-screen flex-col items-center justify-center">
 	<Logo size="text-7xl" />
-	<h2 class="whitespace-nowrap text-4xl font-bold">Investing in Humanity</h2>
+	<h2 class="whitespace-nowrap text-3xl font-bold">Investing in Humanity</h2>
 </main>
