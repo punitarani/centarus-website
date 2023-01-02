f8737622e1df2c3ddf88e526afce1aa77b29d428
Punit Arani <punitsai36@gmail.com>
2023-01-02 14:15:18 -0700

    Reduce GitHub logo size to h-5

--- src/components/footer/footer.svelte
+++ src/components/footer/footer.svelte
@@ -2,7 +2,7 @@
 	<div class="flex h-10 w-10/12 max-w-screen-xl flex-row items-center justify-between">
 		<p>Centarus</p>
 		<a href="https://github.com/punitarani/centarus-website">
-			<img src="icons/github/github-mark-white.png" alt="GitHub logo" class="h-6" />
+			<img src="icons/github/github-mark-white.png" alt="GitHub logo" class="h-5" />
 		</a>
 	</div>
 </footer>
