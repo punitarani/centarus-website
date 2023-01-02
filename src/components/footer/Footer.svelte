eacc630d3b17b58516f3ae56eaef7460af9e6c6a
Punit Arani <punitsai36@gmail.com>
2023-01-01 20:11:59 -0700

    Move text to left

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="flex h-12 flex-col items-center justify-center">
-	<div class="footer-container flex h-10 w-10/12 max-w-screen-xl justify-end">
+	<div class="footer-container flex h-10 w-10/12 max-w-screen-xl justify-start">
 		<p>Centarus</p>
 	</div>
 </footer>
