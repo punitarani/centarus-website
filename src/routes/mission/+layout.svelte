574ab60ba262f3ee9249d01595525a6f84061ab7
Punit Arani <punitsai36@gmail.com>
2023-01-02 12:49:21 -0700

    Create mission page

---
+++ src/routes/mission/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '/src/app.css';
+	import Header from '/src/components/header/header.svelte';
+	import Footer from '/src/components/footer/footer.svelte';
+</script>
+
+<Header />
+<slot />
+<Footer />
