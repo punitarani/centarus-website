0c17316d1bad9a0346c8efdd7f51787a2d5a1258
Punit Arani <punitsai36@gmail.com>
2023-01-02 17:15:42 -0700

    Create basic Mission page

---
+++ src/routes/mission/+page.svelte
@@ -0,0 +1,29 @@
+<div class="flex flex-col items-center">
+	<div class="flex w-full flex-col justify-center">
+		<div class="relative flex h-screen justify-center">
+			<img
+				src="images/stock/Crescent-Earth-rises-above-lunar-horizon.jpg"
+				alt="Crescent Earth rises above lunar horizon"
+				class="absolute top-0 left-0 h-full w-full object-cover"
+			/>
+			<div class="absolute top-12 left-8 lg:inset-x-40 2xl:inset-x-1/4 2xl:top-1/4">
+				<h1 class="text-7xl xl:text-9xl">Open Sourcing</h1>
+				<h1 class="text-7xl xl:text-9xl">the Universe</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	:root {
+		background-color: #070707;
+	}
+
+	h1 {
+		font-family: 'Hubot Sans', 'Mona Sans', sans-serif;
+		font-weight: 500;
+		color: white;
+
+		text-shadow: 0 0 10px rgba(7, 7, 7, 0.25);
+	}
+</style>
