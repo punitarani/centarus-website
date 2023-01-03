8fccb365dbe6235f1187eea507d022c43d91d053
Punit Arani <punitsai36@gmail.com>
2023-01-02 17:03:05 -0700

    Increase footer divider line width to w-full

--- src/components/footer/footer.svelte
+++ src/components/footer/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="mb-4 flex h-12 flex-col items-center justify-center">
-	<hr class="mb-2 h-px w-10/12 max-w-screen-xl rounded border-0 bg-gray-700" />
+	<hr class="mb-2 h-px w-full rounded border-0 bg-gray-700" />
 	<div class="flex h-10 w-10/12 max-w-screen-xl flex-row items-center justify-between px-2">
 		<p>Centarus</p>
 		<a href="https://github.com/punitarani/centarus-website">
